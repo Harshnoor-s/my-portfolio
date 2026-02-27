687738326b8d35cffdc4f2ce2caa979e21df3c07
Harshnoor-Singh-34 <s3943355@student.rmit.edu.au>
2026-02-27 16:09:10 +1100

    added navbar transitions

--- src/components/Intro.jsx
+++ src/components/Intro.jsx
@@ -3,7 +3,7 @@ export default function Intro() {
         <div class=" mb-30">
             <h1 class="font-display text-5xl text-text mb-4 font-bold">Harshnoor Singh</h1>
             <p class="font-display text-lg text-text mb-5">Software Engineer</p>
-            <p class="font-display text-base text-muted">Building thoughtful and functional web experiences while learning and growing as a software engineer.</p>
+            <p class="font-display text-base text-muted">Building thoughtful and functional software while continuously learning and growing as a software engineer.</p>
         </div>
     )
 }
--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -3,10 +3,10 @@ export default function Navbar() {
         <div>
             <nav class="text-display text-muted text-base">
                 <ul>
-                    <li class="mb-2"><a href="#" class="hover:text-hover-accent hover:font-bold">Home</a></li>
-                    <li class="mb-2"><a href="#education" class="mb-2 hover:text-hover-accent hover:font-bold">Education</a></li>
-                    <li class="mb-2"><a href="#projects" class="mb-2 hover:text-hover-accent hover:font-bold">Projects</a></li>
-                    <li class="mb-2"><a href="#experience" class="mb-2 hover:text-hover-accent hover:font-bold">Experience</a></li>
+                    <li class="mb-2"><a href="#" class="hover:text-hover-accent hover:font-bold transition-all duration-200 ease-out">Home</a></li>
+                    <li class="mb-2"><a href="#education" class="mb-2 hover:text-hover-accent hover:font-bold transition-all duration-200 ease-out">Education</a></li>
+                    <li class="mb-2"><a href="#projects" class="mb-2 hover:text-hover-accent hover:font-bold transition-all duration-200 ease-out">Projects</a></li>
+                    <li class="mb-2"><a href="#experience" class="mb-2 hover:text-hover-accent hover:font-bold transition-all duration-200 ease-out">Experience</a></li>
                 </ul>
             </nav>
         </div>
